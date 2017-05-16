a2623d46f446b74ff0bee60fe4141aed8249e868
Josh Dean <josdea@gmail.com>
2017-05-16 14:00:03 -0400

    presentation start

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// Reveal.initialize();
+
+  Reveal.initialize({
+    controls: true,
+    progress: true,
+    // defaultTiming: 120,
+    loop: true,
+    slideNumber: true,
+    autoSlide: 7000,
+    autoSlideStoppable: true,
+    hideAddressBar: true,
+    transition: 'convex',
+    transitionSpeed: 'slow',
+    overview: true,
+  });
